80a95d7be1d5c3ae8b1883a2de40c85692d65d13
Eric-Paul Lecluse <e@epologee.com>
2014-04-21 14:45:48 +0200

    Add new project, for testing using Kiwi

---
+++ Project/OperationCenter/OperationCenter.h
@@ -0,0 +1,13 @@
+//
+//  OperationCenter.h
+//  OperationCenter
+//
+//  Created by Eric-Paul Lecluse on 21-04-14.
+//  Copyright (c) 2014 epologee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OperationCenter : NSObject
+
+@end
---
+++ Project/OperationCenter/OperationCenter.m
@@ -0,0 +1,13 @@
+//
+//  OperationCenter.m
+//  OperationCenter
+//
+//  Created by Eric-Paul Lecluse on 21-04-14.
+//  Copyright (c) 2014 epologee. All rights reserved.
+//
+
+#import "OperationCenter.h"
+
+@implementation OperationCenter
+
+@end
