c4b7481a83774e5f8a95a41e42737a4049a82ff5
Eric-Paul Lecluse <e@epologee.com>
2014-04-21 15:19:32 +0200

    Add a basic spec that demonstrates the lazily injected properties

---
+++ Project/OperationCenterTests/Helpers/TSTLazyPropertiesOperation.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import "EEEOperation.h"
+
+@interface TSTLazyPropertiesOperation : EEEOperation
+
+@property (nonatomic, copy) NSString *theQuickBrownFox;
+@property (nonatomic, copy) NSString *theLazyDog;
+
+@end
