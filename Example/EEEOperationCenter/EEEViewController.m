1eaa39a5619f944d3df221d28d50d70b8ee82aca
Eric-Paul Lecluse <e@epologee.com>
2014-10-01 09:49:54 +0200

    Use the new Cocoapods template for the example and tests project

---
+++ Example/EEEOperationCenter/EEEViewController.m
@@ -0,0 +1,29 @@
+//
+//  EEEViewController.m
+//  EEEOperationCenter
+//
+//  Created by Eric-Paul Lecluse on 10/01/2014.
+//  Copyright (c) 2014 Eric-Paul Lecluse. All rights reserved.
+//
+
+#import "EEEViewController.h"
+
+@interface EEEViewController ()
+
+@end
+
+@implementation EEEViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/EEEOperationCenter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EEEOperationCenter
+//
+//  Created by Eric-Paul Lecluse on 10/01/2014.
+//  Copyright (c) 2014 Eric-Paul Lecluse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EEEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EEEAppDelegate class]));
+    }
+}
